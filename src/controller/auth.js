b793ecc293a5af8f3accc9bf528c61081c47395c
Tan Nguyen <58477197+tandev2k@users.noreply.github.com>
2021-10-03 22:22:14 +0700

    fix oauth2

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -98,16 +98,21 @@ exports.signinWithGoogle = async (req, res) => {
         audience: client_id
     });
     const { name, email, picture } = ticket.getPayload();
-    const userObj = await User.upsert({
-        where: { email: email },
-        update: { name, profilePicture: picture },
-        create: { name, email, profilePicture: picture, username: "GG" + shortid.generate() }
-    })
-    if (userObj) {
-        const { _id, name, email, profilePicture, role } = userObj;
-        const user = { _id, name, email, profilePicture, role };
-        const jwt = generateJwtToken(user._id, user.role);
-        res.status(201).json({ user, token: jwt })
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+        const { _id, name, email, role, profilePicture } = existingUser;
+        const token = generateJwtToken(_id, role);
+        const user = { _id, name, email, role, profilePicture };
+        res.status(201).json({ user, token })
+    } else {
+        const newUser = {
+            name,
+            email,
+            username: "GG" + shortid.generate(),
+            profilePicture: picture
+        }
+        let user = await new User(newUser).save();
+        const token = generateJwtToken(user._id, user.role);
+        res.status(201).json({ user, token })
     }
-    return res.status(400).json({ message: "Something went wrong" })
 }
