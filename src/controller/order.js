e69e35c9a96b52200f5e1ed52ae3ac722868e8b8
Tan Nguyen <58477197+tandev2k@users.noreply.github.com>
2021-11-02 07:48:37 +0700

    add field to get orders

--- src/controller/order.js
+++ src/controller/order.js
@@ -64,11 +64,70 @@ exports.addOrder = (req, res) => {
     });
 }
 
+exports.addOrderByPaymentMomo = (req, res) => {
+    const { items, addressId, totalAmount, paymentStatus, paymentType } = req.body;
+    const orderStatus = [
+        {
+            type: 'ordered',
+            date: new Date(),
+            isCompleted: true,
+        },
+        {
+            type: 'packed',
+            isCompleted: false,
+        },
+        {
+            type: 'shipped',
+            isCompleted: false,
+        },
+        {
+            type: 'delivered',
+            isCompleted: false,
+        },
+    ]
+
+    items.map((item) => {
+        const productId = item.productId;
+        const sizeId = item.sizeId;
+        if (productId) {
+            Cart.updateOne(
+                { user: req.user._id },
+                {
+                    $pull: {
+                        cartItems: {
+                            product: productId,
+                            size: sizeId
+                        },
+                    },
+                }
+            ).exec((error, result) => {
+                if (error)
+                    return res.status(400).json({ error });
+            });
+        }
+    })
+
+    const order = new Order({
+        user: req.user._id,
+        addressId,
+        totalAmount,
+        items,
+        paymentStatus,
+        paymentType,
+        orderStatus
+    })
+    order.save((error, order) => {
+        if (error) return res.status(400).json({ error });
+        if (order) {
+            res.status(201).json({ order });
+        }
+    });
+}
 
 exports.getOrder = (req, res) => {
     const { orderId } = req.body;
     Order.findOne({ _id: orderId })
-        .populate("items.productId", "_id name productPictures")
+        .populate("items.productId", "_id name slug price discountPercent productPictures")
         .populate("items.sizeId", "_id size description")
         .lean()
         .exec((error, order) => {
@@ -107,7 +166,7 @@ exports.updateOrderStatus = (req, res) => {
 
 exports.getCustomerOrders = async (req, res) => {
     const orders = await Order.find({})
-        .populate("items.productId", "name")
+        .populate("items.productId", "_id name slug price discountPercent productPictures")
         .exec();
     res.status(200).json({ orders });
 };
@@ -116,7 +175,7 @@ exports.getCustomerOrders = async (req, res) => {
 exports.getOrders = (req, res) => {
     Order.find({ user: req.user._id })
         .select("_id totalAmount paymentStatus paymentType orderStatus items")
-        .populate("items.productId", "_id name productPictures")
+        .populate("items.productId", "_id name slug price discountPercent productPictures")
         .populate("items.sizeId", "_id size description")
         .exec((error, orders) => {
             if (error) return res.status(400).json({ error });
@@ -129,15 +188,16 @@ exports.getOrders = (req, res) => {
 }
 
 exports.paymentWithMomo = async (req, res) => {
+    const { order } = req.body;
     const partnerCode = "MOMO6K0Y20210317";
     const accessKey = "8oZLaYOOTAswDt0O";
     const secretkey = "MHxk2u6eOXitCarGbCsGXmpydjn0wCAk";
     const requestId = partnerCode + new Date().getTime();
     const orderId = requestId;
     const orderInfo = "Thanh toán giày tại DoubleT";
-    const redirectUrl = "https://localhost:3000/cart";
-    const ipnUrl = "https://localhost:3000/cart";
-    const amount = req.body.amount;
+    const redirectUrl = "https://doublet.vercel.app/cart";
+    const ipnUrl = "https://webhook.site/21039e28-80b1-4c48-a98e-4d06cdadc16d";
+    const amount = order.totalAmount;
     const requestType = "captureWallet"
     const extraData = ""; //pass empty value if your merchant does not have stores
     const rawSignature = "accessKey=" + accessKey + "&amount=" + amount + "&extraData=" + extraData + "&ipnUrl=" + ipnUrl + "&orderId=" + orderId + "&orderInfo=" + orderInfo + "&partnerCode=" + partnerCode + "&redirectUrl=" + redirectUrl + "&requestId=" + requestId + "&requestType=" + requestType
