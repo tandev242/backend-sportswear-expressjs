a34ad55729da7d667e968d08c3d41a444e25c21d
Tan Dev Zai <superjunior242@gmail.com>
2021-11-26 08:26:05 +0700

    refactor statistics fuc

--- src/controllers/statistic.js
+++ src/controllers/statistic.js
@@ -5,119 +5,54 @@ exports.statisticRevenue = async (req, res) => {
     const { type } = req.body;
     const dateFrom = new Date(req.body.dateFrom);
     const dateTo = new Date(req.body.dateTo);
+    var step = -1; //Type param is invalid 
+    // if type === day, getting by yyyy-mm-dd format 
+    if (type == "day") {
+        step = 10;
+    } else if (type == "month") {
+        step = 7;
+    } else if (type == "year") {
+        step = 4
+    }
+    if (step === -1) {
+        return res.status(400).json({ error: "Type param is invalid" })
+    }
     try {
-        if (type == "day") {
-            const revenue = await Order.aggregate(
-                [
-                    {
-                        "$match": {
-                            "createdAt": { "$gte": dateFrom, "$lte": dateTo }
-                        }
-                    },
-                    {
-                        "$group": {
-                            "_id": {
-                                $substr: ['$createdAt', 0, 10]
-                            },
-                            "total": {
-                                "$sum": "$totalAmount"
-                            }
-                        }
-                    },
-                    {
-                        "$project": {
-                            "_id": 0,
-                            "date": "$_id",
-                            "totalAmount": "$total",
-                        }
-                    },
-                    {
-                        "$sort": {
-                            "date": 1
+        const revenue = await Order.aggregate(
+            [
+                {
+                    "$match": {
+                        "createdAt": { "$gte": dateFrom, "$lte": dateTo }
+                    }
+                },
+                {
+                    "$group": {
+                        "_id": {
+                            $substr: ['$createdAt', 0, step]
+                        },
+                        "total": {
+                            "$sum": "$totalAmount"
                         }
                     }
-                ]
-            )
-            if (revenue) {
-                return res.status(200).json({ revenue })
-            }
-            res.status(400).json({ error: "something went wrong" })
-        } else if (type == "month") {
-            const revenue = await Order.aggregate(
-                [
-                    {
-                        "$match": {
-                            "createdAt": { "$gte": dateFrom, "$lte": dateTo }
-                        }
-                    },
-                    {
-                        "$group": {
-                            "_id": {
-                                $substr: ['$createdAt', 0, 7]
-                            },
-                            "total": {
-                                "$sum": "$totalAmount"
-                            }
-                        }
-                    },
-                    {
-                        "$project": {
-                            "_id": 0,
-                            "date": "$_id",
-                            "totalAmount": "$total",
-                        }
-                    },
-                    {
-                        "$sort": {
-                            "date": 1
-                        }
+                },
+                {
+                    "$project": {
+                        "_id": 0,
+                        "date": "$_id",
+                        "totalAmount": "$total",
                     }
-                ]
-            )
-            if (revenue) {
-                return res.status(200).json({ revenue })
-            }
-            res.status(400).json({ error: "something went wrong" })
-        } else if (type == "year") {
-            const revenue = await Order.aggregate(
-                [
-                    {
-                        "$match": {
-                            "createdAt": { "$gte": dateFrom, "$lte": dateTo }
-                        }
-                    },
-                    {
-                        "$group": {
-                            "_id": {
-                                $substr: ['$createdAt', 0, 4]
-                            },
-                            "total": {
-                                "$sum": "$totalAmount"
-                            }
-                        }
-                    },
-                    {
-                        "$project": {
-                            "_id": 0,
-                            "date": "$_id",
-                            "totalAmount": "$total",
-                        }
-                    },
-                    {
-                        "$sort": {
-                            "date": 1
-                        }
+                },
+                {
+                    "$sort": {
+                        "date": 1
                     }
-                ]
-            )
-            if (revenue) {
-                return res.status(200).json({ revenue })
-            }
-            res.status(400).json({ error: "something went wrong" })
-        } else {
-            res.status(400).json({ error: "Syntax is wrong" })
+                }
+            ]
+        )
+        if (revenue) {
+            return res.status(200).json({ revenue })
         }
-
+        res.status(400).json({ error: "something went wrong" })
     } catch (error) {
         res.status(400).json({ error })
     }
